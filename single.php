155aa10de97975f65afa4028bf2f7a82862f99af
supineunicorn <mringer@gmail.com>
2017-01-23 21:35:37 -0500

    initial work on post layout

--- single.php
+++ single.php
@@ -24,12 +24,25 @@ remove_action('genesis_footer', 'genesis_footer_markup_close', 15);
 add_action('genesis_before_content', 'rvamag_before_content');
 function rvamag_before_content() {
 	echo '<div class="rva-before-content"></div>';
-	echo '<div class="cd-gutter-box article  " >';
+	echo '<div class="cd-gutter-box article" >';
 }
 
 add_action('genesis_after_content', 'rvamag_after_content');
 function rvamag_after_content() {
 	echo '</div> <!-- / cd-gutterbox -->';
+}
+
+add_action('genesis_before_entry_content', 'rvamag_after_entry_content');
+function rvamag_after_entry_content() {
+	//echo '<div style="height:40px; background-color:yellow;"> fuck DJT </div>';
+	echo	'<ul class="cd-box cd-icons social-buttons">';
+	echo	'<li class="btn-facebook"><i class="fa fa-facebook" ></i></li>';
+	echo	'<li class="btn-twitter"><i class="fa fa-twitter"></i></li>';
+	echo	'<li class="btn-linkedin"><i class="fa fa-linkedin"></i></li>';
+	echo	'<li class="btn-email"><i class="fa fa-envelope"></i></li>';
+	echo	'<li class="btn-print"><i class="fa fa-print"></i></li>';
+	echo	'<li class="btn-message"><i class="fa fa-comment"></i></li>';
+	echo	'</ul>';
 }
 
 
