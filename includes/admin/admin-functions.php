d802c6b55864340ea8711402dba910c5a05055f3
supineunicorn <mringer@gmail.com>
2017-01-27 13:41:51 -0500

    add photog credit url to media librarty

--- includes/admin/admin-functions.php
+++ includes/admin/admin-functions.php
@@ -212,3 +212,51 @@ function bfg_remove_files_on_upgrade() {
 		unlink(ABSPATH . 'xmlrpc.php');
 
 }
+
+/**
+ * Add Photographer Name and URL fields to media uploader
+ *
+ * @param $form_fields array, fields to include in attachment form
+ * @param $post object, attachment record in database
+ * @return $form_fields, modified form fields
+ */
+ 
+function rva_attachment_field_credit( $form_fields, $post ) {
+	$form_fields['rva-photographer-name'] = array(
+		'label' => 'Photographer Name',
+		'input' => 'text',
+		'value' => get_post_meta( $post->ID, 'rva_photographer_name', true ),
+		'helps' => 'If provided, photo credit will be displayed',
+	);
+
+	$form_fields['rva-photographer-url'] = array(
+		'label' => 'Photographer URL',
+		'input' => 'text',
+		'value' => get_post_meta( $post->ID, 'rva_photographer_url', true ),
+		'helps' => 'Add Photographer URL',
+	);
+
+	return $form_fields;
+}
+
+add_filter( 'attachment_fields_to_edit', 'rva_attachment_field_credit', 10, 2 );
+
+/**
+ * Save values of Photographer Name and URL in media uploader
+ *
+ * @param $post array, the post data for database
+ * @param $attachment array, attachment fields from $_POST form
+ * @return $post array, modified post data
+ */
+
+function rva_attachment_field_credit_save( $post, $attachment ) {
+	if( isset( $attachment['rva-photographer-name'] ) )
+		update_post_meta( $post['ID'], 'rva_photographer_name', $attachment['rva-photographer-name'] );
+
+	if( isset( $attachment['rva-photographer-url'] ) )
+update_post_meta( $post['ID'], 'rva_photographer_url', esc_url( $attachment['rva-photographer-url'] ) );
+
+	return $post;
+}
+
+add_filter( 'attachment_fields_to_save', 'rva_attachment_field_credit_save', 10, 2 );
